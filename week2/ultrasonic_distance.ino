5fd5e79c79dc4e73b1403cc3633ad4db90f39bbc
KimSreng1 <127827934+KimSreng1@users.noreply.github.com>
2025-07-23 20:44:04 +1000

    Create ultrasonic_distance.ino

---
+++ week2/ultrasonic_distance.ino
@@ -0,0 +1,32 @@
+#define TRIG_PIN 9   // D9
+#define ECHO_PIN 10  // D10
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  long duration;
+  float distance;
+
+  // Trigger pulse
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Read echo time
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Convert to distance (cm)
+  distance = duration * 0.034 / 2;
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(100);
+}
